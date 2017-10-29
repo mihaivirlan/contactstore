f46bdc98489fdd0e68c3327f1d5a60d28ed0b572
Mihai <mihaivirlan1@mail.ru>
2017-10-30 00:56:51 +0200

    Initial commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,12 @@
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+let axios = require('axios');
+
+Vue.component('contacts', require('./components/Contacts.vue'));
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="csrf-token" content="{{csrf_token()}}">
+        <title>ContactStore</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div class="container">
+            <div id="app">
+                <contacts></contacts>
+            </div>
+        </div>
+    <script src="/js/app.js"></script>
+    </body>
+</html>
